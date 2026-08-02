15d539577dc9159b3670027ac3ff9df5502aaf69
Priyanshu bhatt <priyanshubhatt130@gmail.com>
2026-08-02 17:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-console.log("Prakash Fast Food Website Ready");
+function calculateOrder(){
+
+    let item = document.getElementById("item");
+    let price = Number(item.value);
+
+    let itemName = item.options[item.selectedIndex].text;
+
+    let quantity = Number(document.getElementById("quantity").value);
+
+    let total = price * quantity;
 
 
-// Explore Menu Button
-
-const exploreButton = document.querySelector(".hero button");
-
-exploreButton.addEventListener("click", function(){
-
-    document.querySelector("#food").scrollIntoView({
-        behavior:"smooth"
-    });
-
-});
+    document.getElementById("total").innerHTML =
+    "Total: ₹" + total;
 
 
-// Simple welcome message
+    let message =
+    "Namaste Prakash Fast Food 🙏%0A%0A" +
+    "Mujhe order karna hai.%0A%0A" +
+    "Item: " + itemName + "%0A" +
+    "Quantity: " + quantity + "%0A" +
+    "Total Amount: ₹" + total + "%0A%0A" +
+    "Please confirm my order.%0A%0A" +
+    "Thank you 😊";
 
-window.onload = function(){
 
-    console.log("Welcome to Prakash Fast Food");
+    document.getElementById("orderWhatsapp").href =
+    "https://wa.me/919917857259?text=" + message;
 
-};
+}
