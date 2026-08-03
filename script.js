9b0b960d723963b7ff1083d73951e47cd36813f4
Priyanshu bhatt <priyanshubhatt130@gmail.com>
2026-08-03 17:57:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let latitude = "";
+let longitude = "";
+
+function getLocation() {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+
+            document.getElementById("locationStatus").innerHTML =
+                "✅ Live Location Added Successfully";
+
+        }, function(error) {
+
+            alert("Please allow location permission.");
+
+        });
+
+    } else {
+
+        alert("Your browser does not support GPS.");
+
+    }
+
+}
+
 function calculateOrder() {
 
     let item = document.getElementById("item");
@@ -14,6 +43,15 @@ function calculateOrder() {
 
     document.getElementById("total").innerHTML = "Total: ₹" + total;
 
+    let locationLink = "";
+
+    if (latitude !== "" && longitude !== "") {
+        locationLink =
+            "https://maps.google.com/?q=" + latitude + "," + longitude;
+    } else {
+        locationLink = "Location Not Shared";
+    }
+
     let message =
 `🍔 *Prakash Fast Food Order*
 
@@ -27,13 +65,20 @@ ${customerAddress}
 📌 Landmark:
 ${landmark}
 
-🍽 Item: ${item.options[item.selectedIndex].text}
+🍽 Item:
+${item.options[item.selectedIndex].text}
 
-🔢 Quantity: ${quantity}
+🔢 Quantity:
+${quantity}
 
-💰 Total: ₹${total}
+💰 Total:
+₹${total}
 
-💳 Payment: ${paymentMethod}`;
+💳 Payment:
+${paymentMethod}
+
+📍 Live Location:
+${locationLink}`;
 
     let whatsappLink =
         "https://wa.me/919634713276?text=" +
@@ -48,11 +93,15 @@ function payNow() {
         document.getElementById("paymentMethod").value;
 
     if (paymentMethod === "Cash on Delivery") {
+
         alert("Cash on Delivery selected.\nPlease place your order on WhatsApp.");
+
         return;
+
     }
 
     alert(
-        "UPI Payment Number:\n\n9634713276\n\nOpen any UPI app (PhonePe, Google Pay, Paytm) and pay to this number."
+        "UPI Payment Number:\n\n9634713276\n\nOpen PhonePe / Google Pay / Paytm and pay to this number."
     );
+
 }
